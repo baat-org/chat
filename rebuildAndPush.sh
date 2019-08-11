fff28ca4971329de72e6a7b0ddd3733e200fb44a
Sachin Goyal <sachin@fanduel.com>
2019-08-11 19:36:58 +0100

    Kill all and then recreate images, oh man it's silly of Docker.

--- rebuildAndPush.sh
+++ rebuildAndPush.sh
@@ -1,10 +1,5 @@
-export SERVICE=chat
-export VERSION=1.0
-
-docker rmi $(docker images -qa -f 'dangling=true')
+docker rmi $(docker images -qa 'sachingoyaldocker/baat-org-chat')
 
 ./gradlew clean build bootJar
-
-docker build --no-cache -t sachingoyaldocker/baat-org-${SERVICE}:${VERSION} . 
-
-docker push sachingoyaldocker/baat-org-${SERVICE}:${VERSION}
+docker build --no-cache -t sachingoyaldocker/baat-org-chat:1.0 . 
+docker push sachingoyaldocker/baat-org-chat:1.0
