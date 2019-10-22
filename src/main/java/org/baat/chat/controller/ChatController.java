75f24ed00c69f4548dc29c7543b2267e7147d6b7
Sachin Goyal <sachin@fanduel.com>
2019-10-22 23:21:29 +0100

    Added logging

--- src/main/java/org/baat/chat/controller/ChatController.java
+++ src/main/java/org/baat/chat/controller/ChatController.java
@@ -1,22 +1,20 @@
 package org.baat.chat.controller;
 
-import org.baat.core.transfer.chat.ChatMessage;
-import org.baat.core.transfer.chat.ChatWSMessage;
-import org.baat.core.transfer.user.UserInfo;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.lang3.BooleanUtils;
 import org.baat.chat.application.ChatApplication;
+import org.baat.core.transfer.chat.ChatMessage;
+import org.baat.core.transfer.chat.ChatWSMessage;
+import org.baat.core.transfer.user.UserInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
@@ -24,78 +22,81 @@ import java.util.Set;
 
 @RestController
 public class ChatController {
+    private final static Logger LOGGER = LoggerFactory.getLogger(ChatController.class);
 
-	@Autowired
-	private RabbitTemplate wsChatMessagePublisher;
+    @Autowired
+    private RabbitTemplate wsChatMessagePublisher;
 
-	@Value("${user_service_uri}")
-	private String userServiceURI;
+    @Value("${user_service_uri}")
+    private String userServiceURI;
 
-	@CrossOrigin
-	@RequestMapping(value = "/", method = RequestMethod.PUT)
-	public void handleMessage(@RequestBody final ChatMessage chatMessage) throws IllegalAccessException, JsonProcessingException {
-		if (chatMessage == null) {
-			throw new IllegalArgumentException("chatMessage must be passed");
-		}
-		if (StringUtils.isEmpty(chatMessage.getSenderUserToken())) {
-			throw new IllegalArgumentException("sender User Token must be passed");
-		}
-		if (StringUtils.isEmpty(chatMessage.getTextMessage())) {
-			throw new IllegalArgumentException("text message must be passed");
-		}
-		if (chatMessage.getRecipientChannelId() == null && chatMessage.getRecipientUserId() == null) {
-			throw new IllegalArgumentException("recipient channel Id or user Id must be passed");
-		}
+    @CrossOrigin
+    @RequestMapping(value = "/", method = RequestMethod.PUT)
+    public void handleMessage(@RequestBody final ChatMessage chatMessage) throws IllegalAccessException, JsonProcessingException {
+        if (chatMessage == null) {
+            throw new IllegalArgumentException("chatMessage must be passed");
+        }
+        if (StringUtils.isEmpty(chatMessage.getSenderUserToken())) {
+            throw new IllegalArgumentException("sender User Token must be passed");
+        }
+        if (StringUtils.isEmpty(chatMessage.getTextMessage())) {
+            throw new IllegalArgumentException("text message must be passed");
+        }
+        if (chatMessage.getRecipientChannelId() == null && chatMessage.getRecipientUserId() == null) {
+            throw new IllegalArgumentException("recipient channel Id or user Id must be passed");
+        }
 
-		if (!validUserToken(chatMessage.getSenderUserToken())) {
-			throw new IllegalAccessException("Invalid sender user token passed");
-		}
+        LOGGER.info("Got a message: {}", chatMessage.getTextMessage());
 
-		final String rawChatWSMessage = createRawChatWSMessage(chatMessage);
+        if (!validUserToken(chatMessage.getSenderUserToken())) {
+            throw new IllegalAccessException("Invalid sender user token passed");
+        }
 
-		if (rawChatWSMessage != null) {
-			wsChatMessagePublisher.convertAndSend( ChatApplication.CHAT_WS_EXCHANGE_NAME, "", rawChatWSMessage);
-		}
-	}
+        final String rawChatWSMessage = createRawChatWSMessage(chatMessage);
 
-	private String createRawChatWSMessage(final ChatMessage chatMessage) throws JsonProcessingException {
-		//TODO handle channelId as well
-		//TODO store message for the receiver (even if no token)
-		final Set<String> recipientUserTokens = findValidUserTokens(chatMessage.getRecipientUserId());
-		if (CollectionUtils.isEmpty(recipientUserTokens)) {
-			return null;
-		}
+        if (rawChatWSMessage != null) {
+            wsChatMessagePublisher.convertAndSend(ChatApplication.CHAT_WS_EXCHANGE_NAME, "", rawChatWSMessage);
+        }
+    }
 
-		final UserInfo senderUserInfo = findUserForToken(chatMessage.getSenderUserToken());
-		if (senderUserInfo == null) {
-			throw new IllegalStateException("Invalid sender user token passed");
-		}
+    private String createRawChatWSMessage(final ChatMessage chatMessage) throws JsonProcessingException {
+        //TODO handle channelId as well
+        //TODO store message for the receiver (even if no token)
+        final Set<String> recipientUserTokens = findValidUserTokens(chatMessage.getRecipientUserId());
+        if (CollectionUtils.isEmpty(recipientUserTokens)) {
+            return null;
+        }
 
-		final ChatWSMessage chatWSMessage = new ChatWSMessage(senderUserInfo.getId(),
-				chatMessage.getRecipientChannelId(), recipientUserTokens, chatMessage.getTextMessage());
+        final UserInfo senderUserInfo = findUserForToken(chatMessage.getSenderUserToken());
+        if (senderUserInfo == null) {
+            throw new IllegalStateException("Invalid sender user token passed");
+        }
 
-		final ObjectMapper mapper = new ObjectMapper();
-		return mapper.writeValueAsString(chatWSMessage);
-	}
+        final ChatWSMessage chatWSMessage = new ChatWSMessage(senderUserInfo.getId(),
+                chatMessage.getRecipientChannelId(), recipientUserTokens, chatMessage.getTextMessage());
 
-	@SuppressWarnings("unchecked")
-	private Set<String> findValidUserTokens(final Long recipientUserId) {
-		return new RestTemplate().getForObject(
-				URI.create(userServiceURI + "/userTokens/" + recipientUserId), Set.class);
-	}
+        final ObjectMapper mapper = new ObjectMapper();
+        return mapper.writeValueAsString(chatWSMessage);
+    }
 
-	private UserInfo findUserForToken(final String userToken) {
-		return new RestTemplate().getForObject(
-				URI.create(userServiceURI + "/userForToken/" + userToken), UserInfo.class);
-	}
+    @SuppressWarnings("unchecked")
+    private Set<String> findValidUserTokens(final Long recipientUserId) {
+        return new RestTemplate().getForObject(
+                URI.create(userServiceURI + "/userTokens/" + recipientUserId), Set.class);
+    }
 
-	private boolean validUserToken(final String userToken) {
-		try {
-			return BooleanUtils.isTrue(new RestTemplate().getForObject(
-					URI.create(userServiceURI + "/validateUserToken/" + userToken), Boolean.class));
-		} catch (Exception exception) {
-			//TODO error logging
-			return false;
-		}
-	}
+    private UserInfo findUserForToken(final String userToken) {
+        return new RestTemplate().getForObject(
+                URI.create(userServiceURI + "/userForToken/" + userToken), UserInfo.class);
+    }
+
+    private boolean validUserToken(final String userToken) {
+        try {
+            return BooleanUtils.isTrue(new RestTemplate().getForObject(
+                    URI.create(userServiceURI + "/validateUserToken/" + userToken), Boolean.class));
+        } catch (Exception exception) {
+            LOGGER.error("Error validating user token {}", userToken, exception);
+            return false;
+        }
+    }
 }
