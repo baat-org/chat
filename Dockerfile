3230111e89a4d394eae2a47d7708b005f342223d
Sachin Goyal <sachin@fanduel.com>
2021-07-02 00:26:20 +0100

    module system

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,8 @@
-FROM adoptopenjdk/openjdk15:alpine-jre
-
-# Dockerfile author / maintainer
+FROM sachingoyaldocker/baat-org-custom-jre15:latest
 MAINTAINER Sachin Goyal <sachin.goyal.se@gmail.com>
+VOLUME /opt
+ARG SERVICE=service
 
-VOLUME /opt/chat
+ADD build/libs/$SERVICE.jar /opt/service/service.jar
 
-ADD build/libs/chat*.jar /opt/chat/chat.jar
-
-ENTRYPOINT ["java","-jar","/opt/chat/chat.jar"]
+ENTRYPOINT ["java","-jar","/opt/service/service.jar"]
---
+++ redeployLocal.sh
@@ -0,0 +1,6 @@
+kubectl delete -f infrastructure/k8s/database-deployment.yml --namespace=baat
+kubectl delete -f infrastructure/k8s/messaging-deployment.yml --namespace=baat
+kubectl delete -f infrastructure/k8s/service-deployment.yml --namespace=baat
+kubectl create -f infrastructure/k8s/database-deployment.yml --namespace=baat
+kubectl create -f infrastructure/k8s/messaging-deployment.yml --namespace=baat
+kubectl create -f infrastructure/k8s/service-deployment.yml --namespace=baat
